de9cda7da9b7673735f77c9b1cbd3f741f0063c5
IT25100330 <it25100330@my.sliit.lk>
2026-05-14 21:48:58 +0530

    Create File Structure
    I created the file structure of our project.

---
+++ OOP-Project-154-Clone/src/main/java/com/hotel/RuSiri/RuSiriApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.RuSiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 		RuSiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuSiriApplication.class, args);
+	}
+
+}
---
+++ OOP-Project-154-Clone/src/test/java/com/hotel/RuSiri/RuSiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.RuSiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuSiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
